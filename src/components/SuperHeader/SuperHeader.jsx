ab03092fdc0631cdb8d21c29a4b2185e99eed541
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-07-02 16:44:58 -0600

    Layout the Superheader (Exercise 1)

--- src/components/SuperHeader/SuperHeader.jsx
+++ src/components/SuperHeader/SuperHeader.jsx
@@ -26,10 +26,18 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+
+  height: 40px;
+  padding-inline: 32px;
+
+  display: flex;
+  gap: 24px;
+  align-items: center;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
