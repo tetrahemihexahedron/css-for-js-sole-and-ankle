fc9aafcb59ece3387f92d671de88781b465b9663
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-07-04 00:45:28 -0600

    Line the text up better

--- src/components/ShoeIndex/ShoeIndex.jsx
+++ src/components/ShoeIndex/ShoeIndex.jsx
@@ -45,6 +45,7 @@ const Wrapper = styled.div`
   flex-direction: row-reverse;
   justify-content: flex-end;
   gap: 32px;
+  align-items: baseline;
 `;
 
 const LeftColumn = styled.div`
@@ -58,6 +59,7 @@ const MainColumn = styled.div`
 const Header = styled.header`
   display: flex;
   justify-content: space-between;
+  align-items: baseline;
 `;
 
 const Title = styled.h2`
